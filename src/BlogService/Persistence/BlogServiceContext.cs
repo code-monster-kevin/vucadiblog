77f7dee13bd9e4e1d57101fd482a49211e20dde7
Kevin Wu Tse Ek <kevin.wu@live.com.my>
2019-05-10 17:51:00 +0800

    added BlogServiceContext

---
+++ src/BlogService/Persistence/BlogServiceContext.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Data;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using BlogService.Models;
+
+namespace BlogService.Persistence
+{
+    public class BlogServiceContext : DbContext
+    {
+        private IDbContextTransaction _currentTransaction;
+        public BlogServiceContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
+        #region Transaction Handling
+        public void BeginTransaction()
+        {
+            if (_currentTransaction != null)
+            {
+                return;
+            }
+
+            if (!Database.IsInMemory())
+            {
+                _currentTransaction = Database.BeginTransaction(IsolationLevel.ReadCommitted);
+            }
+        }
+
+        public void CommitTransaction()
+        {
+            try
+            {
+                _currentTransaction?.Commit();
+            }
+            catch
+            {
+                RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public void RollbackTransaction()
+        {
+            try
+            {
+                _currentTransaction?.Rollback();
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+        #endregion
+    }
+}
